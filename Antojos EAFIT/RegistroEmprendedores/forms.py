c5d5638cd142a18091d5fea881bfddc870726db4
afgarnicar <afgarnicar@eafit.edu.co>
2025-04-10 10:27:51 -0500

    se agrego la opcion de subir una imagen en el formulario, y mas opciones en los horarios

--- Antojos EAFIT/RegistroEmprendedores/forms.py
+++ Antojos EAFIT/RegistroEmprendedores/forms.py
@@ -4,10 +4,14 @@ from .models import Entrepreneur
 class EntrepreneurForm(forms.ModelForm):
     class Meta:
         model = Entrepreneur
-        fields = ['business_name', 'location', 'schedule', 'contact_info']
+        fields = ['business_name', 'location', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'contact_info']
         widgets = {
             'business_name': forms.TextInput(attrs={'class': 'form-control'}),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
-            'schedule': forms.TextInput(attrs={'class': 'form-control'}),
+            'lunes': forms.TextInput(attrs={'class': 'form-control'}),
+            'martes': forms.TextInput(attrs={'class': 'form-control'}),
+            'miercoles': forms.TextInput(attrs={'class': 'form-control'}),
+            'jueves': forms.TextInput(attrs={'class': 'form-control'}),
+            'viernes': forms.TextInput(attrs={'class': 'form-control'}),
             'contact_info': forms.TextInput(attrs={'class': 'form-control'}),
         }   
--- Antojos EAFIT/RegistroEmprendedores/models.py
+++ Antojos EAFIT/RegistroEmprendedores/models.py
@@ -7,8 +7,13 @@ from django.db import models
 class Entrepreneur(models.Model):
     business_name = models.CharField(max_length=255)
     location = models.CharField(max_length=255, blank=True, null=True)
-    schedule = models.CharField(max_length=255, blank=True, null=True)
+    lunes = models.CharField(max_length=255, blank=True, null=True)
+    martes = models.CharField(max_length=255, blank=True, null=True)
+    miercoles = models.CharField(max_length=255, blank=True, null=True)
+    jueves = models.CharField(max_length=255, blank=True, null=True)
+    viernes = models.CharField(max_length=255, blank=True, null=True)
     contact_info = models.CharField(max_length=255, blank=True, null=True)
+    logo = models.ImageField(upload_to='logos/', blank=True, null=True)
 
 def __str__(self):
     return self.business_name
